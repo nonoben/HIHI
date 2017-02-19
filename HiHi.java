834e6398447c8bd5d00cba19b00f99a2a54bad93
AndyChen <h11062002@gmail.com>
2017-02-19 21:31:18 +0800

    Revert "ver3"
    This reverts commit 0f8b16ffe4778ca23b2a19539ee3f85861e687d5.

--- HiHi.java
+++ HiHi.java
@@ -4,7 +4,10 @@ public class HiHi {
 
 	public static void main(String[] args) {
 		for (int i = 1; i <= 10; i++) {
-			System.out.println("*");
+			for (int j = 1; j <= i; j++) {
+				System.out.print("#");
+			}
+			System.out.println();
 		}
 	}
 
