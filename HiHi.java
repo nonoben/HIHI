03bf256331789b47402c5a9193041e682bfda7d2
AndyChen <h11062002@gmail.com>
2017-02-19 21:27:28 +0800

    aa

--- HiHi.java
+++ HiHi.java
@@ -5,7 +5,7 @@ public class HiHi {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 10; i++) {
 			for (int j = 1; j <= i; j++) {
-				System.out.print("*");
+				System.out.print("#");
 			}
 			System.out.println();
 		}
